c9a7f3953fb32a9e17a7aaa48c4b4064b24632e7
RobnettSean22 <robnettsean22@gmail.com>
2020-09-11 01:28:03 +0900

    added a function to check for if a name already exist in the data

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,9 +4,13 @@ let index = 0;
 module.exports = {
   register: async (req, res) => {
     const { username, password, phone_number } = req.body;
-    const foundUser = await userData;
+    const foundUser = await userData.filter(inUse => {
+      return inUse.username.match(username) ? username : null;
+    });
 
     if (foundUser.length) {
+      res.status(400).send(`the username ${username} is already in use`);
+    } else {
       const saltRounds = 12;
       const salt = await bcrypt.genSalt(saltRounds);
       const hashedPassword = await bcrypt.hash(password, salt);
@@ -16,8 +20,6 @@ module.exports = {
         phone_number
       });
       index++;
-    } else {
-      res.status(400).send("user exist");
     }
 
     res.status(200).send(userData);
