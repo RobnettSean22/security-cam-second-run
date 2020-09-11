3690a2fc41d7d449b565b844c3f5a3ce97f2384a
RobnettSean22 <robnettsean22@gmail.com>
2020-09-11 18:26:31 +0900

    totp set and authentification is funtional

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,10 +4,11 @@ const Speakeasy = require("speakeasy");
 const Base64 = require("crypto-js/enc-base64");
 const Utf8 = require("crypto-js/enc-utf8");
 
+let genSecret;
 let index = 0;
 module.exports = {
   register: async (req, res) => {
-    const { email, password, phone_number, alternativePW } = req.body;
+    const { email, password, phone_number } = req.body;
     const foundUser = await userData.filter(inUse => {
       return inUse.email === email ? email : null;
     });
@@ -21,8 +22,7 @@ module.exports = {
       userData.push({
         email,
         password: hashedPassword,
-        phone_number,
-        alternativePW
+        phone_number
       });
       index++;
     }
@@ -47,25 +47,50 @@ module.exports = {
       }
     }
   },
-  sendTOTP: (req, res) => {},
+
+  createToken: async (req, res) => {
+    const { email } = req.body;
+    const [foundUser] = await userData.filter(inUse => {
+      return inUse.email === email ? email : null;
+    });
+    if (!foundUser) {
+      res.status(400).send("user with this account does not exist");
+    }
+    const secret = Speakeasy.generateSecret().ascii;
+    console.log(secret);
+    const sharedSecret = "robnettsean@gmail.com" + secret;
+
+    const token = Speakeasy.totp({
+      secret: sharedSecret,
+
+      algorithm: "sha512",
+      digits: 6
+    });
+
+    genSecret = sharedSecret;
+    console.log(token);
+    res.status(200).send(foundUser);
+  },
+
   alternateLogin: async (req, res) => {
-    const { email, alternativePW } = req.body;
+    const { email, digitCode } = req.body;
     const [foundUser] = await userData.filter(inUse => {
       return inUse.email === email ? email : null;
     });
     if (!foundUser) {
       res.status(400).send("username does not match");
     } else {
-      const asc = "HENNGECHALLENGE003";
-      const sharedSecret = userData.email + asc;
-
+      const secret = Speakeasy.generateSecret().ascii;
+      console.log(secret);
+      console.log(digitCode);
+      console.log(genSecret);
       const authenticated = Speakeasy.totp.verify({
-        secret: sharedSecret,
+        secret: genSecret,
 
-        token: alternativePW,
+        token: digitCode,
 
         algorithm: "sha512",
-        digits: 10
+        digits: 6
       });
       console.log(authenticated);
       if (authenticated) {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const { register, login } = require("./controller/user");
+const {
+  register,
+  login,
+  createToken,
+  alternateLogin
+} = require("./controller/user");
 
 const port = 5000;
 
@@ -8,6 +13,8 @@ app.use(express.json());
 
 app.post("/reg/", register);
 app.post("/login/", login);
+app.post("/createToken/", createToken);
+app.post("/altlogin/", alternateLogin);
 
 app.listen(port, () => {
   console.log(`loud and clear on ${port}`);
